17de9304682516858633d69c0cf6f063649f00d4
Werner Almesberger <werner@almesberger.net>
2013-06-13 20:29:32 -0300

    E407/: provite basic *printf and make demo print numbers
    We need at least this much for debugging output.

--- FreeRTOS/Demo/E407/demo.c
+++ FreeRTOS/Demo/E407/demo.c
@@ -17,6 +17,10 @@
 
 #include "stm32f4xx_conf.h"
 
+#include <stdarg.h>
+#include <stdio.h>
+#include <alloca.h>
+
 
 #define mainFLASH_TASK_PRIORITY		(tskIDLE_PRIORITY + 1UL)
 
@@ -95,11 +99,36 @@ static void print(const char *s)
 }
 
 
+static void printk(const char *fmt, ...)
+{
+	va_list ap;
+	char *buf;
+	int n;
+
+	va_start(ap, fmt);
+	n = vsprintf(NULL, fmt, ap);
+	va_end(ap);
+
+	buf = alloca(n+1);
+//	buf = pvPortMalloc(n+1);
+
+	va_start(ap, fmt);
+	vsprintf(buf, fmt, ap);
+	va_end(ap);
+
+	print(buf);
+
+//	vPortFree(buf);
+}
+
+
 void vParTestToggleLED(unsigned long ulLED)
 {
+	static int n = 0;
+
 	if (ulLED == 0) {
 		GPIOC->ODR ^= 1 << 13;		/* toggle PC13 */
-		print("hello\n");
+		printk("hello %d\n", n++);
 	}
 }
 
--- FreeRTOS/Demo/E407/libc.c
+++ FreeRTOS/Demo/E407/libc.c
@@ -94,13 +94,115 @@ int __sprintf_chk(char *s, int flag, size_t slen, const char *format, ...)
 }
 
 
-/*
- * @@@ __builtin___vsprintf_chk may loop back to __vsprintf_chk. Not sure if
- * this works.
- */
+size_t strlen(const char *s)
+{
+	int n = 0;
+
+	while (*s++)
+		n++;
+	return n;
+}
+
+
+static int log_calc(unsigned v, unsigned base)
+{
+	int n = 0;
+
+	if (!v)
+		return 1;
+	while (v) {
+		v /= base;
+		n++;
+	}
+	return n;
+}
+
+
+static void log_put(char *s, unsigned v, unsigned base)
+{
+	if (!v) {
+		s[-1] = '0';
+		return;
+	}
+	while (v) {
+		*--s = "0123456789abcdef"[v % base];
+		v /= base;
+	}
+}
+
 
 int __vsprintf_chk(char *s, int flag, size_t slen, const char *format,
     va_list ap)
 {
-	return __builtin___vsprintf_chk(s, flag, slen, format, ap);
+//	const char *end = s+slen;
+	int n = 0, len;
+	int int_val;
+	unsigned uint_val;
+	const char *str_val;
+
+	while (*format) {
+		if (*format == '%') {
+			switch (*++format) {
+			case '%':
+			default:
+				if (s)
+					*s++ = *format;
+				n++;
+				break;
+			case 'd':
+				int_val = va_arg(ap, int);
+				len = int_val < 0 ? 1+log_calc(-int_val, 10) :
+				    log_calc(int_val, 10);
+				n += len;
+				if (!s)
+					break;
+				if (int_val < 0) {
+					*s = '-';
+					log_put(s+len, -int_val, 10);
+				} else {
+					log_put(s+len, int_val, 10);
+				}
+				s += len;
+				break;
+			case 'p':
+				/* fall through. only works on 32 bit arch */
+			case 'u':
+				uint_val = va_arg(ap, unsigned);
+				len = log_calc(uint_val, 10);
+				n += len;
+				if (!s)
+					break;
+				s += len;
+				log_put(s, uint_val, 10);
+				break;
+			case 's':
+				str_val = va_arg(ap, const char *);
+				len = strlen(str_val);
+				n += len;
+				if (!s)
+					break;
+				memcpy(s, str_val, len);
+				s += len;
+				break;
+			case 'x':
+				uint_val = va_arg(ap, unsigned);
+				len = log_calc(uint_val, 16);
+				n += len;
+				if (!s)
+					break;
+				s += len;
+				log_put(s, uint_val, 16);
+				break;
+			}
+			format++;
+			continue;
+		}
+		if (s)
+			*s++ = *format;
+		format++;
+		n++;
+	}
+	if (s)
+		*s = 0;
+	return n;
 }
