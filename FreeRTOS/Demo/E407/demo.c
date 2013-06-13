b6daeda14627489eadc05fc9533b8937841401fd
Werner Almesberger <werner@almesberger.net>
2013-06-12 23:39:15 -0300

    E407/: enable UART6 and print "hello" each time we toggle the LED
    known bug: the bit rate the hardware uses is half the rate requested

--- FreeRTOS/Demo/E407/demo.c
+++ FreeRTOS/Demo/E407/demo.c
@@ -23,14 +23,37 @@
 
 int main(void)
 {
+	static USART_InitTypeDef uart_init = {
+		.USART_BaudRate		= 38400,
+		.USART_WordLength	= USART_WordLength_8b,
+		.USART_StopBits		= USART_StopBits_1,
+		.USART_Parity		= USART_Parity_No,
+		.USART_Mode		= USART_Mode_Tx,
+		.USART_HardwareFlowControl = USART_HardwareFlowControl_None,
+	};
+	static GPIO_InitTypeDef gpio_init = {
+		.GPIO_Pin	= GPIO_Pin_6,
+		.GPIO_Mode	= GPIO_Mode_AF,
+		.GPIO_Speed	= GPIO_Speed_2MHz,
+		.GPIO_OType	= GPIO_OType_PP,
+		.GPIO_PuPd	= GPIO_PuPd_NOPULL,
+	};
+
 	/* Ensure all priority bits are assigned as preemption priority bits. */
 
 	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_4);
 
 //	vParTestInitialise();
 
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIOCEN;	/* enable clock to GPIOC */
+	RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOC, ENABLE);
 	GPIOC->MODER = 1 << 26;			/* make PC13 an output */
+
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART6, ENABLE);
+	GPIO_PinAFConfig(GPIOC, GPIO_PinSource6, GPIO_AF_USART6);
+	GPIO_Init(GPIOC, &gpio_init);
+
+	USART_Init(USART6, &uart_init);
+	USART_Cmd(USART6, ENABLE);
 
 	vStartLEDFlashTasks(mainFLASH_TASK_PRIORITY);
 	vTaskStartScheduler();
@@ -39,10 +62,29 @@ int main(void)
 }
 
 
+static void put_char(char c)
+{
+	while (USART_GetFlagStatus(USART6, USART_FLAG_TXE) == RESET);
+	USART_SendData(USART6, c);
+}
+
+
+static void print(const char *s)
+{
+	while (*s) {
+		if (*s == '\n')
+			put_char('\r');
+		put_char(*s++);
+	}
+}
+
+
 void vParTestToggleLED(unsigned long ulLED)
 {
-	if (ulLED == 0)
+	if (ulLED == 0) {
 		GPIOC->ODR ^= 1 << 13;		/* toggle PC13 */
+		print("hello\n");
+	}
 }
 
 
